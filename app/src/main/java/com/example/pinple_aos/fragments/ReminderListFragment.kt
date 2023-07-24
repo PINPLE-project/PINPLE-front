aa83190a21bc2d9088332a6dbb33f6e5b0343d25
alschlee <1daysixmeal@gmail.com>
2023-07-24 16:56:13 +0900

    [MOD] 설정된 알림 페이지 RecyclerView로 수정 #1

--- app/src/main/java/com/example/pinple_aos/fragments/ReminderListFragment.kt
+++ app/src/main/java/com/example/pinple_aos/fragments/ReminderListFragment.kt
@@ -6,9 +6,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.pinple_aos.R
-import com.google.android.material.bottomsheet.BottomSheetBehavior
-import android.widget.FrameLayout
-import android.widget.TextView
+import com.example.pinple_aos.ReminderItem
+import com.example.pinple_aos.ReminderAdapter
+import androidx.recyclerview.widget.RecyclerView
 
 class ReminderListFragment : Fragment() {
 
@@ -25,7 +25,24 @@ class ReminderListFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
         return inflater.inflate(R.layout.fragment_reminder_list, container, false)
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val itemList = ArrayList<ReminderItem>()
+
+        itemList.add(ReminderItem("반포한강공원","2023년 5월 28일 (일) 17:00"))
+        itemList.add(ReminderItem("반포한강공원","2023년 5월 28일 (일) 17:00"))
+
+        val reminderAdapter = ReminderAdapter(itemList)
+
+        val recyclerView = view.findViewById<RecyclerView>(R.id.rv_board)
+        recyclerView.adapter = reminderAdapter
+
+    }
 }
+
 
