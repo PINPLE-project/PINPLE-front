b0f5be40220d09d0e842b9baebbb990d9b99692d
alschlee <1daysixmeal@gmail.com>
2023-08-02 15:54:05 +0900

    [FEAT] 네이버 지도 띄우기 #8

--- app/src/main/java/com/example/pinple_aos/fragments/MainFragment.kt
+++ app/src/main/java/com/example/pinple_aos/fragments/MainFragment.kt
@@ -6,8 +6,17 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.pinple_aos.R
+import com.naver.maps.map.NaverMapSdk
 
 
 class MainFragment : Fragment(R.layout.fragment_main) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // 네이버 지도 API 키 설정
+        NaverMapSdk.getInstance(requireContext()).client =
+            NaverMapSdk.NaverCloudPlatformClient("8w319xk099")
+    }
 
 }
