32c1489dd5d59256d33edcae59a10e3500c99f2e
alschlee <1daysixmeal@gmail.com>
2023-08-02 22:56:04 +0900

    [FEAT] 핀뷰, 데이터뷰 전환 구현 #8

--- app/src/main/java/com/example/pinple_aos/fragments/MainPinFragment.kt
+++ app/src/main/java/com/example/pinple_aos/fragments/MainPinFragment.kt
@@ -7,18 +7,17 @@ import android.view.View
 import android.view.ViewGroup
 import com.example.pinple_aos.R
 import com.naver.maps.map.NaverMapSdk
-import com.example.pinple_aos.databinding.FragmentMainBinding
+import com.example.pinple_aos.databinding.FragmentMainPinBinding
 
-
-class MainFragment : Fragment(R.layout.fragment_main) {
-    private var _binding: FragmentMainBinding? = null
+class MainPinFragment : Fragment(R.layout.fragment_main_pin) {
+    private var _binding: FragmentMainPinBinding? = null
     private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        _binding = FragmentMainBinding.inflate(inflater, container, false)
+        _binding = FragmentMainPinBinding.inflate(inflater, container, false)
         return binding.root
     }
 
@@ -29,11 +28,11 @@ class MainFragment : Fragment(R.layout.fragment_main) {
         NaverMapSdk.getInstance(requireContext()).client =
             NaverMapSdk.NaverCloudPlatformClient("8w319xk099")
 
-        // 데이터뷰 버튼 클릭 시 이벤트 처리
-        binding.dataViewButton.setOnClickListener {
-            val mainPinFragment = MainPinFragment()
+        // 핀뷰 버튼 클릭 시 이벤트 처리
+        binding.pinViewButton.setOnClickListener {
+            val mainFragment = MainFragment()
             parentFragmentManager.beginTransaction()
-                .replace(R.id.fragmentContainer, mainPinFragment)
+                .replace(R.id.fragmentContainer, mainFragment)
                 .addToBackStack(null)
                 .commit()
         }
@@ -43,5 +42,4 @@ class MainFragment : Fragment(R.layout.fragment_main) {
         super.onDestroyView()
         _binding = null
     }
-
 }
