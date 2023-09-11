827557da83c8f433ccf0324fd74fe5d19c8fd2c1
alschlee <1daysixmeal@gmail.com>
2023-09-11 13:54:26 +0900

    [NETWORK] 로그인, 회원가입 API 호출 코드 작성하기 #28

--- app/src/main/java/com/example/pinple_aos/Retrofit2/RetrofitClient.kt
+++ app/src/main/java/com/example/pinple_aos/Retrofit2/RetrofitClient.kt
@@ -1,5 +1,7 @@
 package com.example.pinple_aos.Retrofit2
 
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
@@ -11,9 +13,19 @@ object RetrofitClient {
 
     fun getClient(): Retrofit? {
         if (retrofit == null) {
+            // HttpLoggingInterceptor 생성 및 설정
+            val loggingInterceptor = HttpLoggingInterceptor()
+            loggingInterceptor.level = HttpLoggingInterceptor.Level.BODY
+
+            // OkHttpClient에 로깅 인터셉터 추가
+            val httpClient = OkHttpClient.Builder()
+                .addInterceptor(loggingInterceptor)
+                .build()
+
             retrofit = Retrofit.Builder()
                 .baseUrl(BASE_URL)
                 .addConverterFactory(GsonConverterFactory.create())
+                .client(httpClient) // 커스텀 OkHttpClient 설정
                 .build()
         }
         return retrofit
